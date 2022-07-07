80acda82dad50255e35db6cd9236290dd799777f
wvanKane <31739955+wvanKane@users.noreply.github.com>
2022-07-07 13:31:51 -0400

    Add files via upload

--- Assignment1.cs
+++ Assignment1.cs
@@ -13,37 +13,25 @@ namespace _02UnderstandingTypes
         {
             //please fill a number between 0 to 255
             //int max = 10; 
-            //for (byte i = 0; i < max; i++)
-            //{
-                int randomNumber = new Random().Next(3) + 1;  // generates a random number between 1 and 3 
+            for (byte hour = 0; hour < 24; hour++)
+            {
 
-                while (true)
-                {
-                    Console.WriteLine("Please enter a number: ");
-                    double input = Convert.ToDouble(Console.ReadLine());
-                    if (input == randomNumber)
-                    {
-                        Console.WriteLine("Corrent!! the number is " + randomNumber);
-                        return;
-                    }
-                    else if (input > randomNumber)
-                    {
-                        if (input > 3)
-                        {
-                            Console.WriteLine("Please enter number less than 3.");
-                        }
-                        Console.WriteLine("Your number is greater than corrent number");
-                    }
-                    else
-                    {
-                        if (input < 1)
-                        {
-                            Console.WriteLine("Please enter number greater than 1.");
-                        }
-                        Console.WriteLine("Your number is less than corrent number");
-                    }
-                }
-            //}
+            //String timeNow = DateTime.Now.ToString("t", CultureInfo.CreateSpecificCulture("es-ES"));
+            //DateTime current = DateTime.Now;
+            //byte hour = byte.Parse(current.Hour.ToString());
+            //int hour = 7;
+            //if (6 < hour < 12)
+            if ((hour > 6) && (hour <= 12))
+                Console.WriteLine("Good morning");
+            if ((hour > 12) && (hour <= 18))
+                Console.WriteLine("Good afternoon");
+            if ((hour > 18) && (hour <= 21))
+                Console.WriteLine("Good evening");
+            if ((hour > 21) || (hour <= 6))
+                Console.WriteLine("Good night");
+
+
+            }
         }
 
         //private static void WriteLine(byte i)
@@ -145,6 +133,52 @@ namespace _02UnderstandingTypes
                     Console.WriteLine("Your number is less than corrent number");
                 }
             }
+        }
+
+        public void print_a_Pyramid()
+        {
+            Console.WriteLine("Please enter an intger: ");
+            int max = Convert.ToInt32(Console.ReadLine());
+            //int max = 15;
+            for (int i = max; i >= 0; i -= 2)
+            {
+                for (int k = 0; k <= i; k += 2)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = max; j >= i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void birth_date()
+        {
+            Console.WriteLine("Please enter your birthday(MM/DD/YYYY): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            int days = (DateTime.Today - date).Days;
+            Console.WriteLine($"You are now {days} days old.");
+            int daysToNextAnniversary = 10000 - (days % 10000);
+            Console.WriteLine($"There are {daysToNextAnniversary} day(s) left to the next anniversy day");
+            DateTime anniversaryDate = DateTime.Today.AddDays(10000 - (days % 10000));
+            Console.WriteLine($"AnniversaryDate is {anniversaryDate}");
+        }
+
+        public void Greeting()
+        {
+            DateTime current = DateTime.Now;
+            byte hour = byte.Parse(current.Hour.ToString());
+            if ((hour > 21) && (hour <= 6))
+                Console.WriteLine("Good night");
+            if ((hour > 6) && (hour <= 12))
+                Console.WriteLine("Good morning");
+            if ((hour > 12) && (hour <= 18))
+                Console.WriteLine("Good afternoon");
+            if ((hour > 18) && (hour <= 21))
+            //if (18 < hour <= 20)
+                Console.WriteLine("Good evening");
         }
     }   
 }
